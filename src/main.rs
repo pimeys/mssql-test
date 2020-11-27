3918a581327d6e969ebd2e2fcff44206b3a99655
Julius de Bruijn <julius+github@nauk.io>
2020-11-27 14:27:55 +0100

    Simplify config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use async_std::net::TcpStream;
 use quaint::{prelude::*, single::Quaint};
-use tiberius::{AuthMethod, Client, Config, EncryptionLevel};
+use tiberius::{Client, Config};
+
+static CONN_STR: &str = "jdbc:sqlserver://localhost:1433;database=master;user=SA;password=<YourStrong@Passw0rd>;trustServerCertificate=true;";
 
 static QUERY: &str = r#"
     DECLARE @now datetime;
@@ -9,27 +11,17 @@ static QUERY: &str = r#"
         @SampleRequestNumber = 2533055,
         @RequestTimeStamp = @now,
         @ToState = 'Cancelled',
-        @Comment = 'Stop Sampling Orchard Hazard added'"#;
+        @Comment = 'Stop Sampling Orchard Hazard added'
+"#;
 
 async fn t_query() -> anyhow::Result<()> {
-    let mut config = Config::new();
-
-    config.host("localhost");
-    config.port(1433);
-    config.authentication(AuthMethod::sql_server("SA", "<YourStrong@Passw0rd>"));
-    config.trust_cert();
-    config.encryption(EncryptionLevel::On);
+    let config = Config::from_jdbc_string(CONN_STR)?;
 
     let tcp = TcpStream::connect(config.get_addr()).await?;
     tcp.set_nodelay(true)?;
 
     let mut client = Client::connect(config, tcp).await?;
-
-    let res = client
-        .query(QUERY, &[&"index_renaming_must_work_when_renaming_to_custom"])
-        .await?
-        .into_results()
-        .await?;
+    let res = client.query(QUERY, &[]).await?.into_results().await?;
 
     dbg!(res);
 
@@ -37,11 +29,8 @@ async fn t_query() -> anyhow::Result<()> {
 }
 
 async fn q_query() -> anyhow::Result<()> {
-    let conn = Quaint::new("jdbc:sqlserver://localhost:1433;database=master;user=SA;password=<YourStrong@Passw0rd>;trustServerCertificate=true;").await?;
-
-    let res = conn
-        .query_raw(QUERY, &["index_renaming_must_work_when_renaming_to_custom".into()])
-        .await?;
+    let conn = Quaint::new(CONN_STR).await?;
+    let res = conn.query_raw(QUERY, &[]).await?;
 
     dbg!(res);
 
